fe889e0892be161c2ffdd56a715482871689c4aa
JosiahSia28 <josiahsiachua@gmail.com>
2025-12-15 23:02:55 +0800

    update

--- src/main/java/com/angularspring/angularspring/service/TaskService.java
+++ src/main/java/com/angularspring/angularspring/service/TaskService.java
@@ -2,40 +2,30 @@ package com.angularspring.angularspring.service;
 
 import com.angularspring.angularspring.entity.Task;
 import com.angularspring.angularspring.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class TaskService {
-    private final TaskRepository taskRepository;
 
-    public TaskService(TaskRepository taskRepository) {
-        this.taskRepository = taskRepository;
-    }
+    @Autowired
+    private TaskRepository taskRepository;
 
-    public Task createTask(Task task) {
-        return taskRepository.save(task);
-    }
-
-    public List<Task> getAllTasks() {
-        return taskRepository.findAll();
-    }
-
-    public Optional<Task> getTaskById(Long id) {
-        return taskRepository.findById(id);
-    }
-
-    public Task updateTask(Task task) {
-        return taskRepository.save(task);
-    }
-
-    public void deleteTask(Long id) {
-        taskRepository.deleteById(id);
-    }
-
+    // Get all tasks for a specific user
     public List<Task> getTasksByUserId(String userId) {
         return taskRepository.findByUserId(userId);
+    }
+
+    // Add a new task for a user
+    public Task addTask(String userId, Task task) {
+        task.setUserId(userId);  // make sure userId is set
+        return taskRepository.save(task);
+    }
+
+    // Remove a task by its ID
+    public void removeTask(long taskId) {
+        taskRepository.deleteById(taskId);
     }
 }
